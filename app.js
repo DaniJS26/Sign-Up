7bf45116f69329d4dd1ea4100ea09381f0ab1d53
Dani JS <levi@JS.local>
2023-06-22 20:58:04 +0630

    getting userData with bodyParser

--- app.js
+++ app.js
@@ -10,7 +10,15 @@ app.get("/", (req, res) => {
     res.sendFile(__dirname + "/index.html");
 })
 
-
+app.post("/", (req, res) => {
+    let fName = req.body.fName;
+    let lName = req.body.lName;
+    let email = req.body.email;
+    res.write(fName)
+    res.write(lName)
+    res.write(email)
+    res.send()
+})
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000...")
